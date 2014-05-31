3c39dc26e59e0fc94e46382c488b3e7a3544f0c8
Tim Shelburne <shelburt02@gmail.com>
2014-05-31 13:54:16 -0500

    created from_hash method on the Pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,7 +3,14 @@ class Pet < ActiveRecord::Base
   belongs_to :image
 
   def self.from_hash(hash)
-  	shelter = Shelter.find_or_create_by(name: hash.delete(:shelter_name))
-  	image 	
+  	unless hash[:shelter_name].blank?
+	  	shelter = Shelter.find_or_create_by(name: hash.delete(:shelter_name))  
+	  	hash.merge! shelter: shelter
+	  end
+
+  	image = Image.from_pet_id(hash[:id])
+  	hash.merge! image: image unless image.blank?
+
+  	Pet.new(hash)
   end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Pet do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	describe '::from_hash' do 
+	
+		it "will add a persisted shelter" do
+			pet = Pet.from_hash(shelter_name: 'Test Shelter')
+			Shelter.where(name: 'Test Shelter').should be_exists
+			pet.shelter.should eq Shelter.where(name: 'Test Shelter').first
+		end
+
+		it "will not add a shelter if no shelter name is given" do
+			pet = Pet.from_hash(id: '1234')
+			pet.shelter.should be_nil
+		end
+
+		it "will generate an image" do
+			pet = Pet.from_hash(id: "1234")
+			pet.image.url.should eq 'http://www.petharbor.com/get_image.asp?RES=Detail&ID=1234&LOCATION=ASTIN'
+		end
+	
+	end
+
 end
