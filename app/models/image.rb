7949bd27b316f0e184d2f5c32faa5daa9905a891
Tim Shelburne <shelburt02@gmail.com>
2014-05-31 13:43:26 -0500

    added methods for generating images to the image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,19 @@
 class Image < ActiveRecord::Base
+
+	BASE_URL   = 'http://www.petharbor.com/get_image.asp'
+	RESOLUTION = 'Detail'
+	LOCATION   = 'ASTIN'
+
+	def self.from_pet_id(id)
+		Image.new(path: "?RES=#{RESOLUTION}&ID=#{id}&LOCATION=#{LOCATION}")
+	end
+
+	def url
+		"#{BASE_URL}#{path}"
+	end
+
+	def to_s
+		url
+	end
+
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Image do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	describe '::from_pet_id' do 
+	
+		it "will set the url" do
+			Image.from_pet_id('A037822').url.should eq 'http://www.petharbor.com/get_image.asp?RES=Detail&ID=A037822&LOCATION=ASTIN'
+		end
+	
+	end
+
+	describe '#url' do 
+	
+		it "will return the path appended to the url base" do
+			Image.new(path: '?var=test').url.should eq 'http://www.petharbor.com/get_image.asp?var=test'
+		end
+	
+	end
+
+	describe '#to_s' do 
+	
+		it "will return the url of the image" do
+			Image.new(path: '?var=test').to_s.should eq 'http://www.petharbor.com/get_image.asp?var=test'
+		end
+	
+	end
+
 end
