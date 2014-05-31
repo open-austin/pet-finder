372ebb8b7f37e4d05533aa06a2d293fe935ecc5c
Tim Shelburne <shelburt02@gmail.com>
2014-05-31 13:17:32 -0500

    added color to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,9 @@
 class Pet < ActiveRecord::Base
   belongs_to :shelter
   belongs_to :image
+
+  def self.from_hash(hash)
+  	shelter = Shelter.find_or_create_by(name: hash.delete(:shelter_name))
+  	image 	
+  end
 end
