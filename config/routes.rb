7a113eb9325654f84cb1c5cca043853dc217f2b6
AnnieH <ming.ting@gmail.com>
2014-06-01 10:40:37 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   post 'populator/update', to: 'pet_populator#update', as: 'populate'
   post 'populator/reconcile', to: 'pet_populator#reconcile', as: 'reconcile'
-  get 'pets/search', to: 'pets_controller#search', as: 'search'
-  get 'pets/results', to: 'pets_controller#results', as: 'results'
-  get 'pets/:id', to: 'pets_controller#show', as: 'show'
-  post 'pets/results/subscribe', to: 'pets_controller#subscribe', as: 'subscribe'
+  get 'pets/search', to: 'pets#search', as: 'search'
+  get 'pets/results', to: 'pets#results', as: 'results'
+  get 'pets/:id', to: 'pets#show', as: 'show'
+  post 'pets/results/subscribe', to: 'pets#subscribe', as: 'subscribe'
   
   root to: 'pets#search'
 end
