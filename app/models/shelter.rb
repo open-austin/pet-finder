3ca7393001108ed6c1b8e43c8ca662c3fad881d9
Tim Shelburne <shelburt02@gmail.com>
2014-06-01 15:41:57 -0500

    added map link to pet show template

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,11 @@
 class Shelter < ActiveRecord::Base
 	attr_accessible :name
+
+	def map_link
+		"https://www.google.com/maps/preview?safe=off&q=#{ERB::Util.url_encode name}"
+	end
+
+	def to_s
+		name
+	end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe Shelter do
 
+	describe '#map_link' do 
+	
+		it "will return a google link based on the name" do
+			Shelter.new(name: 'Austin Animal Center').map_link.should eq 'https://www.google.com/maps/preview?safe=off&q=Austin%20Animal%20Center'
+		end
+	
+	end
+
+	describe '#to_s' do 
+	
+		it "will return the name of the shelter" do
+			Shelter.new(name: 'Austin Animal Center').to_s.should eq 'Austin Animal Center'
+		end
+	
+	end
+
 end
