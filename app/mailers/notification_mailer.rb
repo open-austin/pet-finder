05c1a3176c99aeda9f084b79957c2fd6cdf4d62d
AnnieH <ming.ting@gmail.com>
2014-05-31 20:32:04 -0500

    change email content, text now using plivo api

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,14 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "petfinderatx@gmail.com"
+
+  def welcome_email(user)
+  	@user = user
+  	mail(to: @user.email, subject: 'Welcome to the PetFinder Newsfeed!')
+  end
+
+  def notify_email(user, pet = nil)
+    @user = user
+    # @pet = pet
+    mail(to: @user.email, subject: 'You Have a New PetFinder Match!')
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,10 +4,23 @@ class Notification < ActiveRecord::Base
 
 	attr_accessible :type
 
-	def send_email
+	def send_welcome_email
+		NotificationMailer.welcome_email(user).deliver
+	end
+
+	def send_notify_email
+		NotificationMailer.notify_email(user).deliver
 	end
 
 	def send_text
+		uri = URI.parse("https://api.plivo.com/v1/Account/#{ENV['PLIVO_AUTH_ID']}/Message/")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.basic_auth(ENV['PLIVO_AUTH_ID'],ENV['PLIVO_AUTH_TOKEN'])
+    
+		request.set_form_data({"src" => ENV['PLIVO_NUMBER'], "dst" => user.phone_number, "text" => "New Pet Found! (via PetFinder app)"})
+    response = http.request(request)
 	end
 
 end
