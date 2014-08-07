f90fd7053b6dda2e3acf62d3dcf53e8a28060f31
Tim Shelburne <shelburt02@gmail.com>
2014-08-06 23:25:46 -0400

    updated the pet populator controller with authentication

---
+++ spec/support/helpers/auth_helper.rb
@@ -0,0 +1,5 @@
+module AuthHelper
+  def http_login(username, password)
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(username,password)
+  end  
+end
