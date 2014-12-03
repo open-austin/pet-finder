d140c640f9acb17683864fe078e7a0b129c477ec
Tim Shelburne <shelburt02@gmail.com>
2014-12-03 11:12:55 -0600

    updated image repository to check the image exists, and use a default image if not

--- app/services/image_repository.rb
+++ app/services/image_repository.rb
@@ -4,12 +4,15 @@ require 'securerandom'
 class ImageRepository
 
 	def self.store(image_url, suggestion="image")
-		filename = "#{SecureRandom.uuid}--#{suggestion}.jpg"
-
-		file = bucket.objects[filename] or raise "Cannot create key - #{filename}"
-		file.write download_image(image_url), acl: :public_read or raise "Cannot write to file - #{filename}"
+		response = download_image image_url
 		
-		file.public_url.to_s
+		if is_valid_image? response
+			image = FileHandler.safe_name "#{suggestion}.jpg"
+			image.write response
+		else
+			image = FileHandler.new '_default/pet-avatar.png'
+		end
+		image.url
 	end
 
 	private
@@ -18,12 +21,42 @@ class ImageRepository
 		Net::HTTP.get URI(image_url)
 	end
 
-	def self.s3
-		AWS::S3.new
+	def self.is_valid_image?(response)
+		response.length > 1000
 	end
 
-	def self.bucket
-		s3.buckets[Figaro.env.s3_bucket] or raise "Bucket not found - #{Figaro.env.s3_bucket}"
+	class FileHandler
+
+		def initialize(filename)
+			@file = file_from filename
+		end
+
+		def self.safe_name(filename)
+			self.new "#{SecureRandom.uuid}--#{filename}"
+		end
+
+		def url
+			@file.public_url.to_s
+		end
+
+		def write(content)
+			@file.write content, acl: :public_read or raise "Cannot write to file - #{filename}"
+		end
+
+		private 
+
+		def s3
+			AWS::S3.new
+		end
+
+		def bucket
+			s3.buckets[Figaro.env.s3_bucket] or raise "Bucket not found - #{Figaro.env.s3_bucket}"
+		end
+
+		def file_from(filename)
+			bucket.objects[filename] or raise "Cannot create key - #{filename}"
+		end
+
 	end
 
 end
